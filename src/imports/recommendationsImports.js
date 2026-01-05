1e687868bf186775b0e8920438bd2117375985e8
ekoumpar <cdkhelen@gmail.com>
2026-01-05 23:33:16 +0200

    improve maintainability of recommendations

---
+++ src/imports/recommendationsImports.js
@@ -0,0 +1,23 @@
+// React imports
+export { useEffect, useState } from 'react';
+export { Link, useNavigate } from 'react-router-dom';
+
+// Custom hooks
+export { default as useAuth } from '../hooks/useAuth';
+export { default as useNavigationWithClearFilters } from '../hooks/useNavigationWithClearFilters';
+
+// API imports
+export { getUserRecommendations } from '../api/users';
+export { getAllWorks } from '../api/works';
+
+// Components
+export { WorkGridSkeleton } from '../components/Skeleton';
+export { default as HomeCarousel } from '../components/HomeCarousel';
+
+// Utils
+export { default as logger } from '../utils/logger';
+export {
+  extractWorksFromResponse,
+  normalizeWorks,
+  shuffleArray,
+} from '../utils/normalize';
